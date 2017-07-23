03db6523e8d11c3719bef2cca9ac18d273350402
dnajafi <dbnajafi@gmail.com>
2017-07-23 12:55:19 -0700

    add nodemon and morgan (for logging)

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,8 +3,28 @@ let User = require('../models/user');
 module.exports = function(app) {
 
 	app.get('/', function(req, res) {
+		res.render('index.ejs');
+	});
 
-		res.send("Hello world!");
+	app.get('/signup', function(req, res) {
+
+		res.render('signup.ejs', { message: 'Victory' });
+
+	});
+
+	app.post('/signup', function(req, res) {
+
+		let newUser = new User();
+		newUser.local.username = req.body.email;
+		newUser.local.password = req.body.password;
+
+		newUser.save(function(err) {
+			if(err) {
+				throw err;
+			}
+		});
+
+		res.redirect('/');
 
 	});
 
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ let cookieParser = require('cookie-parser');
 let session = require('express-session');
 let morgan = require('morgan');
 let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+let path = require('path');
 
 let configDB = require('./config/database.js');
 mongoose.connect(configDB.url)
@@ -13,10 +15,16 @@ mongoose.connect(configDB.url)
 /* Middlewares */
 app.use(morgan('dev')); // middleware for logging
 app.use(cookieParser());// parse every cookie in the header and place into req.cookies variable
+app.use(bodyParser.urlencoded({extended: true})); // puts form data into a req.body object
 // creates req.session variable
 app.use(session({secret: 'mysecret', 
 	saveUninitialized: true, 
-	resave: true})); // saveUninitialized saves unitialized user sessions to db; resave saves user session even if nothings changed
+	resave: true})); // saveUninitialized saves unitialized user sessions to db; resave saved user session even if nothings changed
+
+
+app.set('view engine', 'ejs');// set the view engine/templating engine
+app.set('views', path.join(__dirname, '/views')); // manually set where the views are
+
 
 // app.use('/', function(req, res) {
 // 	res.send('Hit express program');
